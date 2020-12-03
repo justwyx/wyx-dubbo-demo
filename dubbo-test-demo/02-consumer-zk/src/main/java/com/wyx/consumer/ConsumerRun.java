9476ae8825115fbd48604c3292014fff62735946
yuxiang_wen <wenyvxiang@iiasaas.com>
2020-12-03 14:46:09 +0800

    add:负载均衡

--- dubbo-test-demo/02-consumer-zk/src/main/java/com/wyx/consumer/ConsumerRun.java
+++ dubbo-test-demo/02-consumer-zk/src/main/java/com/wyx/consumer/ConsumerRun.java
@@ -12,7 +12,9 @@ public class ConsumerRun {
 	public static void main(String[] args) {
 		ApplicationContext ac = new ClassPathXmlApplicationContext("spring-consumer.xml");
 		SomeService service = (SomeService) ac.getBean("someService");
-		String hello = service.hello("China");
-		System.out.println(hello);
+		for (int i = 0; i < 10; i++) {
+			String hello = service.hello("China");
+			System.out.println(hello);
+		}
 	}
 }
--- dubbo-test-demo/02-provider-zk02/src/main/java/com/wyx/provider/SomeServiceImpl.java
+++ dubbo-test-demo/02-provider-zk02/src/main/java/com/wyx/provider/SomeServiceImpl.java
@@ -10,8 +10,8 @@ public class SomeServiceImpl implements SomeService {
 
 	@Override
 	public String hello(String name) {
-		System.out.println(name + "：我是第一个服务提供者，");
-		return "我是第一个服务提供者，Hello Dubbo World! " + name;
+		System.out.println(name + "：我是第二个服务提供者");
+		return "我是第二个服务提供者，Hello Dubbo World! " + name;
 	}
 
 }
